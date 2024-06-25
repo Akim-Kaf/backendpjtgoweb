8c565265f69908a8a9069cc20b97d52b5cda49a3
Akim-Kaf <akimkafanga@gmail.com>
2024-06-25 22:14:26 +0200

    add of secret key

--- server.js
+++ server.js
@@ -1,4 +1,13 @@
 const mongoose=require('mongoose');
+const dotenv=require('dotenv');
+var express= require('express');
+var bodyParser= require('body-parser');
+
+var app= express()
+// charge les variables d'environnement depuis le fichier .env
+dotenv.config();
+
+const SECRET_KEY = process.env.SECRET_KEY;
 
 const getDataFromXlsx=require('./GetData/getDataFromXlsx');
 var XLSX = require("xlsx");
@@ -36,29 +45,45 @@ async function saveData(){
 }
 
 async function getDomainesData(){    
-    const domaines = await DomaineBd.find();
+    const domaines = await DomaineBd.find().limit(6);
     console.log("Async Data get done");
     
     return domaines;        
 }
 
-const dataSet=getDataFromXlsx(workbook);
+//const dataSet=getDataFromXlsx(workbook);
 //console.log("Dataset: ",dataSet);
-saveData();
+//saveData();
 
-var express= require('express');
-var app= express()
-var bodyParser= require('body-parser');
 
-//Middleware
+
+
+//Middlewares
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: false}));
 // parse application/json
 app.use(bodyParser.json());
 var cors = require('cors');
 app.use(cors());
-//Routes
-app.get('/projetgoweb/domaines',(request, response)=>{
+
+function authentification(req,res,next){
+    const authHeader=req.headers['authorization'];
+    const token= authHeader && authHeader.split(' ')[1];
+    console.log("Token receve:",token);
+    console.log("SECRET_KEY:",SECRET_KEY);
+    console.log("test:",token===SECRET_KEY);
+    console.log("Token receve len:",token.length);
+    console.log("SECRET_KEY len :",SECRET_KEY.length);
+    if(token == null)return res.sendStatus(401);
+
+    if(token!==SECRET_KEY){
+        return res.sendStatus(403);
+    }
+    next();
+}
+
+//Routes securise par le middleware d'authentification
+app.get('/projetgoweb/domaines',authentification,(request, response)=>{
     
     console.log("GET DOMAINES!!!!");
     getDomainesData().then((reponsedata)=>{
@@ -67,9 +92,10 @@ app.get('/projetgoweb/domaines',(request, response)=>{
     });    
 })
 
-app.post('/projetgoweb/intervention',(request,response)=>{
+app.post('/projetgoweb/intervention',authentification,(request,response)=>{
     console.log("Request posted: ",request.body);
 })
+
 app.listen(8080);
 console.log("Listen to the port 8080 ..");
 
